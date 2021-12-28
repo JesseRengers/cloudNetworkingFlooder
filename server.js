2e2b6387dec31534e85d41f01affdafe979dd979
Jesse Rengers <j.j.rengers@student.utwente.nl>
2021-12-28 17:35:48 +0100

    no hardcoded values

--- server.js
+++ server.js
@@ -1,24 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const app = express();
-//const http = require("http")
-const dgram = require('dgram')
-
-// var dgram = require("dgram"),
-//     Commander  = require('commander'),
-//     cluster = require('cluster'),
-//     os = require('os'),
-//     idleGC = require('idle-gc'),
-//     q = require('q');
-
-//data that gets send to the packet sink
-msg = new Buffer ([
-    0x08, 0x00, 0x43, 0x52, 0x00, 0x01, 0x0a, 0x09,
-    0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
-    0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f, 0x70,
-    0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x61,
-    0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69]
-);
 
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,15 +11,16 @@ app.get("/", function(req,res) {
     res.send("This is the flooder")
 })
 
-//create a really large dummy string as data traffic
-const data = 'x'.repeat(1024)
-
 //React on the post request of the client at /flooder, only if key is a specific string
 //runs the flood function
 app.post('/flooder',function(req,res) {
     console.log("Got a post")
     if (req.body.key == 'ditiseensoortwachtwoord') {
-        instruct_flooder(req.body.offset,req.body.duration) //launc flooder function
+        instruct_flooder(req.body.offset,
+            req.body.sink_ip,
+            req.body.sink_port,
+            req.body.packet_size,
+            req.body.duration) //launc flooder function
         res.writeHead(200, {'Content-Type': 'text/html'})
         res.end('Now flooding after ' + req.body.offset + ' for ' + req.body.duration + ' seconds')
     } else {
@@ -47,16 +30,9 @@ app.post('/flooder',function(req,res) {
     }
 })
 
-function sendOne (ip,port) {
-    return new Promise(function(resolev,reject){
-        client.send(msg,0,msg.length,port,ip,function(err) {
-            return err ? reject(err) : resolve()
-        })
-    })
-}
 
 //Waits <offset> seconds before flooding a host in the options constant
-const instruct_flooder = async function(offset,duration) {
+const instruct_flooder = async function(offset,sink_ip,sink_port,packet_size,duration) {
     //Waiting
     console.log("now waiting for " + offset + " seconds")
     await new Promise(resolve => setTimeout(resolve, offset * 1000));
@@ -65,14 +41,12 @@ const instruct_flooder = async function(offset,duration) {
     const spawn = require("child_process").spawn;
 
     //Python arguments: packet_bin_ip, packet_bin_port, packet_size, duration
-    const pythonProcess = spawn('python3',["flooder.py", '192.168.2.13', 161, 50,duration]);
+    const pythonProcess = spawn('python3',["flooder.py", sink_ip, sink_port, packet_size,duration]);
 
 
     pythonProcess.stdout.on('data', (data) => {
-        console.log(data)
+        console.log(data.toString('hex'))
     });
-
-    console.log("Done")
 }
 
 
